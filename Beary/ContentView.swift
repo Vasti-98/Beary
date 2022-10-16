83f875366438b08584fafd0a27696d9d5e33e7bf
Sadhika Akula <sadhika.akula@berkeley.edu>
2022-10-16 00:01:59 -0700

    first changes

--- Beary/ContentView.swift
+++ Beary/ContentView.swift
@@ -8,15 +8,54 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State private var showDetails = false
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
-        }
-        .padding()
+        ZStack {
+            Rectangle().fill(Color("Green"))
+            VStack { //small border
+                
+                Image("logo")
+                    .resizable()
+                    .frame(width: 150, height: 150)
+                    .position(CGPoint(x: 180, y: 190))
+                
+                Spacer().frame(height: 200)
+                
+                Text("**beary**").multilineTextAlignment(.center)
+                    .font(.largeTitle)
+                    .foregroundColor(Color("Red"))
+                    .position(CGPoint(x: 200, y: 80))
+                
+                
+                //SignUp
+                RoundedRectangle(cornerRadius: 25)
+                    .fill(Color("Beige"))
+                    .frame(width: 130, height: 50)
+                    .position(CGPoint(x: 195, y: 140))
+                Button("Sign Up") {
+                    print("Loggin")
+                }
+                .position(CGPoint(x: 195, y: 44))
+                .foregroundColor(Color.black)
+                
+                //logiN
+                RoundedRectangle(cornerRadius: 25)
+                    .fill(Color("Beige"))
+                    .frame(width: 130, height: 50)
+                Button("Log In") {
+                    print("logging in")
+                }
+                .position(CGPoint(x: 195, y: -32))
+                .foregroundColor(Color.black)
+                
+                
+                Spacer().frame(height: 50)
+                // make a Button() instead
+            }
+        }.background(Color("Green")) //filling
+        
     }
+   
 }
 
 struct ContentView_Previews: PreviewProvider {
