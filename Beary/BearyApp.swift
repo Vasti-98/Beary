16d9a2fb13b14e7f1e0408406680c4ead90f3fdc
Sadhika Akula <sadhika.akula@berkeley.edu>
2022-10-15 22:24:09 -0700

    Initial Commit

---
+++ Beary/BearyApp.swift
@@ -0,0 +1,17 @@
+//
+//  BearyApp.swift
+//  Beary
+//
+//  Created by Sadhika Akula on 10/15/22.
+//
+
+import SwiftUI
+
+@main
+struct BearyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
